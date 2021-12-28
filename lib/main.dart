2c78128126b795aa858611401cd5246b9a1ea7a1
SK OWNER <gundiavaumixture@gmail.com>
2021-12-28 19:27:57 +0530

    day2  compleated

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_config/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  // int day = 30;
+  // String name = "shourya";
+  // double pi = 3.14;
+  // bool male = true;
+  // num temp = 30.5; //num can take int and double value
 
+  // var week_day = "tuesday";
+  // const pi = 3.14;
+
+//press ctrl + .      to wrap a conta
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("welcome to 30 days flutter"),
-          ),
-        ),
-      ),
+      home: Homepage(),
     );
   }
 }
